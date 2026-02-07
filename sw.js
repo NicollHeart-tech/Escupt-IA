21a29b70e3595c9eb4b02d40663ec46bb0bda1cf
NicollHeart-tech <nikinhoxv9@gmail.com>
2026-02-07 14:48:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker simples para permitir a instalação do PWA
+self.addEventListener('fetch', function(event) {
+    // Aqui você poderia adicionar cache, mas vamos deixar simples por enquanto
+});
