83896d8da4ed0fd0b7229314ad9a787788a9732d
NicollHeart-tech <nikinhoxv9@gmail.com>
2026-02-08 16:11:10 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// sw.js - Versão de Força Bruta para Instalação
+const CACHE_NAME = 'escupt-cache-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './logo.png' // Garanta que este nome está idêntico ao arquivo
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
