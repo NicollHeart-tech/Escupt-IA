e3dabeebde42e63ba80a5a39ba233b58645e5676
NicollHeart-tech <nikinhoxv9@gmail.com>
2026-02-08 16:07:12 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Service Worker simples para permitir a instalação do PWA
-self.addEventListener('fetch', function(event) {
-    // Aqui você poderia adicionar cache, mas vamos deixar simples por enquanto
-});
