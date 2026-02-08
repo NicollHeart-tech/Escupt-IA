20239949313c7a50cf0d17d7e19e09549fa10bc0
NicollHeart-tech <nikinhoxv9@gmail.com>
2026-02-08 19:32:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-// Service Worker simples para permitir a instalação do PWA
-self.addEventListener('fetch', function(event) {
-    // Aqui você poderia adicionar cache, mas vamos deixar simples por enquanto
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    // O Chrome exige que o evento fetch seja ouvido para validar o PWA
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return new Response("Offline");
+        })
+    );
 });
